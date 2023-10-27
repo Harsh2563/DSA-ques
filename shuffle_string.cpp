68ebc1045063392a8494a99438e1a6ada75e3525
Harsh2563 <139123224+Harsh2563@users.noreply.github.com>
2023-10-27 13:56:33 +0530

    Update shuffle_string.cpp

--- shuffle_string.cpp
+++ shuffle_string.cpp
@@ -1,1 +1,12 @@
-
+string restoreString(string s, vector<int>& indices) {
+        int n = indices.size();
+        vector<char> v(n);
+        for(int i = 0;i<n;i++) {
+            v[indices[i]] = s[i];
+        }
+        string ans="";
+        for(int i = 0;i<n;i++) {
+            ans+=v[i];
+        }
+        return ans;
+    }
